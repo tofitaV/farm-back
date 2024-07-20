28f47e7e7380c891ed3b9e6931153a13884441c6
tofita1337 <tofita1337@gmail.com>
2024-07-20 15:55:40 +0300

    add data validation

--- src/main/java/com/example/happyfarmer/ValidateData.java
+++ src/main/java/com/example/happyfarmer/ValidateData.java
@@ -3,26 +3,28 @@ package com.example.happyfarmer;
 import com.example.happyfarmer.Models.*;
 import com.example.happyfarmer.Repositories.DepotRepository;
 import com.example.happyfarmer.Repositories.UserRepository;
+import org.apache.commons.codec.digest.HmacUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
 @CrossOrigin(maxAge = 3600)
-@Controller
 @RestController
 public class ValidateData {
-    private static final String BOT_TOKEN = System.getProperty("botToken");
 
+    private static final String BOT_TOKEN = System.getProperty("botToken");
     private final UserRepository userRepository;
     private final DepotRepository depotRepository;
 
@@ -32,19 +34,14 @@ public class ValidateData {
         this.depotRepository = depotRepository;
     }
 
-    @RequestMapping(value = "/authorize", method = RequestMethod.POST)
+    @PostMapping("/authorize")
     public ResponseEntity<?> validateData(@RequestBody TelegramUserInfo telegramUserInfo) {
         InitDataUnsafe initDataUnsafe = telegramUserInfo.getInitDataUnsafe();
-        String receivedHash = initDataUnsafe.getHash();
         String refCode = initDataUnsafe.getStart_param();
 
-        String dataCheckString = createDataCheckString(initDataUnsafe);
-        String secretKey = generateSecretKey(BOT_TOKEN);
-
-        String computedHash = computeHmacSHA256(dataCheckString.getBytes(StandardCharsets.UTF_8), secretKey.getBytes(StandardCharsets.UTF_8));
-        //if (computedHash.equals(receivedHash)) {
-        if (true) {
-            try {
+        TelegramAuth telegramAuth = new TelegramAuth();
+        try {
+            if (telegramAuth.isValid(telegramUserInfo.getInitData(), BOT_TOKEN)) {
                 TelegramUser telegramUser = initDataUnsafe.getUser();
                 Users user = userRepository.findByTelegramId(telegramUser.getId());
 
@@ -55,12 +52,12 @@ public class ValidateData {
                     Users newUser = createNewUser(telegramUser, refCode);
                     return ResponseEntity.ok(newUser.getTelegramId());
                 }
-            } catch (Exception e) {
-                e.printStackTrace();
-                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Invalid hash");
             }
-        } else {
-            return ResponseEntity.ok(294367378);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
         }
     }
 
@@ -88,7 +85,7 @@ public class ValidateData {
         }
 
         userRepository.save(newUser);
-        if (!depotRepository.existsByUserId(newUser.getTelegramId())){
+        if (!depotRepository.existsByUserId(newUser.getTelegramId())) {
             depotRepository.save(Account.builder().userId(newUser.getTelegramId()).build());
         }
         return newUser;
@@ -104,54 +101,5 @@ public class ValidateData {
         } catch (NumberFormatException e) {
             throw new NumberFormatException("Cannot parse referral code");
         }
-    }
-
-    private String createDataCheckString(InitDataUnsafe initDataUnsafe) {
-        TreeMap<String, String> sortedMap = new TreeMap<>();
-        sortedMap.put("auth_date", initDataUnsafe.getAuth_date());
-        sortedMap.put("hash", initDataUnsafe.getHash());
-        sortedMap.put("query_id", initDataUnsafe.getQuery_id());
-        TelegramUser user = initDataUnsafe.getUser();
-        if (user != null) {
-            sortedMap.put("allows_write_to_pm", String.valueOf(user.getAllows_write_to_pm()));
-            sortedMap.put("first_name", user.getFirst_name());
-            sortedMap.put("id", String.valueOf(user.getId()));
-            sortedMap.put("is_premium", String.valueOf(user.getIs_premium()));
-            sortedMap.put("language_code", user.getLanguage_code());
-            sortedMap.put("last_name", user.getLast_name());
-            sortedMap.put("username", user.getUsername());
-        }
-        StringBuilder dataCheckString = new StringBuilder();
-        for (Map.Entry<String, String> entry : sortedMap.entrySet()) {
-            dataCheckString.append(entry.getKey()).append("=").append(entry.getValue()).append("\n");
-        }
-        return dataCheckString.toString();
-    }
-
-    private String generateSecretKey(String botToken) {
-        return computeHmacSHA256(botToken.getBytes(StandardCharsets.UTF_8), "WebAppData".getBytes(StandardCharsets.UTF_8));
-    }
-
-    private String computeHmacSHA256(byte[] data, byte[] key) {
-        try {
-            SecretKeySpec secretKeySpec = new SecretKeySpec(key, "HmacSHA256");
-            Mac mac = Mac.getInstance("HmacSHA256");
-            mac.init(secretKeySpec);
-            byte[] hmacData = mac.doFinal(data);
-            return bytesToHex(hmacData);
-        } catch (NoSuchAlgorithmException | InvalidKeyException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private String bytesToHex(byte[] bytes) {
-        StringBuilder hexString = new StringBuilder();
-        for (byte aByte : bytes) {
-            String hex = Integer.toHexString(0xff & aByte);
-            if (hex.length() == 1) hexString.append('0');
-            hexString.append(hex);
-        }
-        return hexString.toString();
     }
 }
